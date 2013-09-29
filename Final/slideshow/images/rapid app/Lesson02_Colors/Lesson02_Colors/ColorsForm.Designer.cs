f6477104a146e003c52f8d313be0e3486ea4a7d1
mitchtracy <mitchtracy11@gmail.com>
2013-09-29 17:49:57 -0400

    Final
    Everything is done

---
+++ Final/slideshow/images/rapid app/Lesson02_Colors/Lesson02_Colors/ColorsForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Lesson02_Colors
+{
+    partial class ColorsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBox2 = new System.Windows.Forms.GroupBox();
+            this.WhiteRadioButton = new System.Windows.Forms.RadioButton();
+            this.BlackRadioButton = new System.Windows.Forms.RadioButton();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.GrayRadioButton = new System.Windows.Forms.RadioButton();
+            this.YellowRadioButton = new System.Windows.Forms.RadioButton();
+            this.BlueRadioButton = new System.Windows.Forms.RadioButton();
+            this.BeigeRadioButton = new System.Windows.Forms.RadioButton();
+            this.GroupBox2.SuspendLayout();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBox2
+            // 
+            this.GroupBox2.Controls.Add(this.WhiteRadioButton);
+            this.GroupBox2.Controls.Add(this.BlackRadioButton);
+            this.GroupBox2.Location = new System.Drawing.Point(12, 153);
+            this.GroupBox2.Name = "GroupBox2";
+            this.GroupBox2.Size = new System.Drawing.Size(145, 65);
+            this.GroupBox2.TabIndex = 3;
+            this.GroupBox2.TabStop = false;
+            this.GroupBox2.Text = "Select Text Color";
+            // 
+            // WhiteRadioButton
+            // 
+            this.WhiteRadioButton.AutoSize = true;
+            this.WhiteRadioButton.Location = new System.Drawing.Point(6, 39);
+            this.WhiteRadioButton.Name = "WhiteRadioButton";
+            this.WhiteRadioButton.Size = new System.Drawing.Size(53, 17);
+            this.WhiteRadioButton.TabIndex = 1;
+            this.WhiteRadioButton.Text = "&White";
+            this.WhiteRadioButton.UseVisualStyleBackColor = true;
+            this.WhiteRadioButton.CheckedChanged += new System.EventHandler(this.WhiteRadioButton_CheckedChanged);
+            // 
+            // BlackRadioButton
+            // 
+            this.BlackRadioButton.AutoSize = true;
+            this.BlackRadioButton.Checked = true;
+            this.BlackRadioButton.Location = new System.Drawing.Point(6, 16);
+            this.BlackRadioButton.Name = "BlackRadioButton";
+            this.BlackRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.BlackRadioButton.TabIndex = 0;
+            this.BlackRadioButton.TabStop = true;
+            this.BlackRadioButton.Text = "Blac&k";
+            this.BlackRadioButton.UseVisualStyleBackColor = true;
+            this.BlackRadioButton.CheckedChanged += new System.EventHandler(this.BlackRadioButton_CheckedChanged);
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.GrayRadioButton);
+            this.GroupBox1.Controls.Add(this.YellowRadioButton);
+            this.GroupBox1.Controls.Add(this.BlueRadioButton);
+            this.GroupBox1.Controls.Add(this.BeigeRadioButton);
+            this.GroupBox1.Location = new System.Drawing.Point(12, 35);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(145, 112);
+            this.GroupBox1.TabIndex = 2;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Select  Form Color";
+            // 
+            // GrayRadioButton
+            // 
+            this.GrayRadioButton.AutoSize = true;
+            this.GrayRadioButton.Location = new System.Drawing.Point(6, 85);
+            this.GrayRadioButton.Name = "GrayRadioButton";
+            this.GrayRadioButton.Size = new System.Drawing.Size(47, 17);
+            this.GrayRadioButton.TabIndex = 3;
+            this.GrayRadioButton.Text = "&Gray";
+            this.GrayRadioButton.UseVisualStyleBackColor = true;
+            this.GrayRadioButton.CheckedChanged += new System.EventHandler(this.GrayRadioButton_CheckedChanged);
+            // 
+            // YellowRadioButton
+            // 
+            this.YellowRadioButton.AutoSize = true;
+            this.YellowRadioButton.Location = new System.Drawing.Point(6, 62);
+            this.YellowRadioButton.Name = "YellowRadioButton";
+            this.YellowRadioButton.Size = new System.Drawing.Size(56, 17);
+            this.YellowRadioButton.TabIndex = 2;
+            this.YellowRadioButton.Text = "&Yellow";
+            this.YellowRadioButton.UseVisualStyleBackColor = true;
+            this.YellowRadioButton.CheckedChanged += new System.EventHandler(this.YellowRadioButton_CheckedChanged);
+            // 
+            // BlueRadioButton
+            // 
+            this.BlueRadioButton.AutoSize = true;
+            this.BlueRadioButton.Location = new System.Drawing.Point(6, 39);
+            this.BlueRadioButton.Name = "BlueRadioButton";
+            this.BlueRadioButton.Size = new System.Drawing.Size(46, 17);
+            this.BlueRadioButton.TabIndex = 1;
+            this.BlueRadioButton.Text = "B&lue";
+            this.BlueRadioButton.UseVisualStyleBackColor = true;
+            this.BlueRadioButton.CheckedChanged += new System.EventHandler(this.BlueRadioButton_CheckedChanged);
+            // 
+            // BeigeRadioButton
+            // 
+            this.BeigeRadioButton.AutoSize = true;
+            this.BeigeRadioButton.Checked = true;
+            this.BeigeRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.BeigeRadioButton.Name = "BeigeRadioButton";
+            this.BeigeRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.BeigeRadioButton.TabIndex = 0;
+            this.BeigeRadioButton.TabStop = true;
+            this.BeigeRadioButton.Text = "&Beige";
+            this.BeigeRadioButton.UseVisualStyleBackColor = true;
+            this.BeigeRadioButton.CheckedChanged += new System.EventHandler(this.BeigeRadioButton_CheckedChanged);
+            // 
+            // ColorsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 274);
+            this.Controls.Add(this.GroupBox2);
+            this.Controls.Add(this.GroupBox1);
+            this.Name = "ColorsForm";
+            this.Text = "Change Colors";
+            this.Load += new System.EventHandler(this.ColorsForm_Load);
+            this.GroupBox2.ResumeLayout(false);
+            this.GroupBox2.PerformLayout();
+            this.GroupBox1.ResumeLayout(false);
+            this.GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.GroupBox GroupBox2;
+        internal System.Windows.Forms.RadioButton WhiteRadioButton;
+        internal System.Windows.Forms.RadioButton BlackRadioButton;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+        internal System.Windows.Forms.RadioButton GrayRadioButton;
+        internal System.Windows.Forms.RadioButton YellowRadioButton;
+        internal System.Windows.Forms.RadioButton BlueRadioButton;
+        internal System.Windows.Forms.RadioButton BeigeRadioButton;
+    }
+}
+
---
+++ Final/slideshow/images/rapid app/Lesson02_MessageFormatter/Lesson02_MessageFormatter/MessageForm.cs
@@ -0,0 +1,110 @@
+﻿// Project:       Message Formatter
+// Programmer:    Tom Tsiliopoulos
+// Date:          September 2013
+// Description:   This project uses labels, text boxes, radio buttons,
+// 	              a check box, images in picture boxes, and buttons 
+//  			  to change the display.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson02_MessageFormatter
+{
+    public partial class MessageForm : Form
+    {
+        public MessageForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MessageForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DisplayButton_Click(object sender, EventArgs e)
+        {
+            // Display the text in the message area.
+
+            MessageLabel.Text = NameTextBox.Text + ":  " + MessageTextBox.Text;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+         // Clear the text controls.
+
+        var NTB = NameTextBox;
+            // Clear the text box.
+            NTB.Clear();
+            // Reset the insertion point.
+            NTB.Focus();
+
+        MessageTextBox.Clear();
+        MessageLabel.Text = "";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            // Exit program
+            this.Close();
+        }
+
+        private void BlackRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Make the label black.
+
+            MessageLabel.ForeColor = Color.Black;
+        }
+
+        private void BlueRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Make the label blue.
+
+            MessageLabel.ForeColor = Color.Blue;
+        }
+
+        private void GreenRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Make the label Green.
+
+            MessageLabel.ForeColor = Color.Green;
+        }
+
+        private void RedRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Make the label Red.
+
+            MessageLabel.ForeColor = Color.Red;
+        }
+
+        private void BigPictureBox_Click(object sender, EventArgs e)
+        {
+        // Switch the icon.
+
+        BigPictureBox.Visible = false;
+        LittlePictureBox.Visible = true;
+        }
+
+        private void LittlePictureBox_Click(object sender, EventArgs e)
+        {
+            // Switch the icon.
+
+        LittlePictureBox.Visible = false;
+        BigPictureBox.Visible = true;
+        }
+
+        private void VisibleCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // Set the visibility for messageLabel.
+
+            MessageLabel.Visible = VisibleCheckBox.Checked;
+        }
+    }
+}
